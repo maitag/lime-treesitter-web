8188490387100d3d519c6c111b931e77e4e64263
Sylvio Sell <info@maitag.de>
2023-04-18 23:58:29 +0200

    to let test js AND glsl

--- src/Main.hx
+++ src/Main.hx
@@ -19,35 +19,54 @@ class Main extends lime.app.Application {
 		
 		// invoking tree-sitter.js and its .wasm
 		js.Syntax.code("
+		
 			const Parser = window.TreeSitter;
+			
 			Parser.init().then( () => {
-				const parser = new Parser;
-				// loading the language grammar (js into this case)
-				const glsl = Parser.Language.load('lib/tree-sitter-glsl.wasm').then ( (GLSL) => {
-					parser.setLanguage(GLSL);
-					this.treeSitterIsReady(parser);
+				
+				
+				// loading the javascript grammar
+				const jsParser = new Parser;
+				Parser.Language.load('lib/tree-sitter-glsl.wasm').then ( (language) => {
+					jsParser.setLanguage(language);
+					this.treeSitterJS(jsParser);
 				});
+				
+				
+				const glslParser = new Parser;
+				// loading the glsl grammar
+				Parser.Language.load('lib/tree-sitter-glsl.wasm').then ( (language) => {
+					glslParser.setLanguage(language);
+					this.treeSitterGLSL(glslParser);
+				});
+				
+				
 			});
+			
 		");
 		
 	}
 	
+	// callbacks after the language-parsers is ready to use
 	
-	public function treeSitterIsReady(parser:Dynamic) {
+	// ----- testing javascript parsing ---------
+	
+	public function treeSitterJS(parser:Dynamic) {
 		
 		// trace("ready", parser);
 		
 		var sourceCode = 'let x = 1; console.log(x);';
 		
 		var tree = parser.parse(sourceCode);
-		//js.Syntax.code("console.log(tree);");
 		
+		//js.Syntax.code("console.log(tree);");
 		trace( tree.rootNode.toString() );
 		
 		var callExpression = tree.rootNode.child(1).firstChild;
 		trace(callExpression);
 
 		
+		// change something inside source
 		var newSourceCode = 'let x = "a"; console.log(x);';
 
 		tree.edit({
@@ -63,7 +82,21 @@ class Main extends lime.app.Application {
 		trace( newTree.rootNode.toString() );
 	}
 	
+
+	// ----- testing glsl parsing ---------
 	
+	public function treeSitterGLSL(parser:Dynamic) {
+		
+		// trace("ready", parser);
+		
+		var sourceCode = 'vec3 xyz;';
+		
+		var tree = parser.parse(sourceCode);
+		
+		//js.Syntax.code("console.log(tree);");
+		trace( tree.rootNode.toString() );
+	
+	}
 	
 	
 }
